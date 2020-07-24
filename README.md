b175558a620c30763a72070d716f59d349e02979
Mike Santoro <mike.santoro42@gmail.com>
2020-07-23 22:34:22 -0400

    basic http server running locally

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-## Chess Line
+# Chess Line
 
 Chess application built using Node.js
 
-# Tech Stack
-Node.js -- Version 12.18.3
+## Tech Stack
+Node.js --  Version 12.18.3
+Express --  Version 4.17.1
---
+++ app.js
@@ -0,0 +1,22 @@
+// MJS 07-23-2020
+// Application Entry Point
+
+// express for the webserver 
+var express = require('express')
+var app = express()
+
+// serve static file 
+app.use(express.static('public'))
+
+// http server
+var http = require('http').Server(app)
+var port = process.env.PORT || 3000 
+
+// default route 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/default.html');
+});
+
+http.listen(port, function() {
+    console.log('listening on *: ' + port);
+});
