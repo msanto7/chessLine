aaa53269a91815a6385e31788a5726ac93dc3ad0
Mike Santoro <mike.santoro42@gmail.com>
2020-08-01 23:18:35 -0400

    adding the html, css, and js to display the board from the library

---
+++ public/default.js
@@ -0,0 +1,25 @@
+
+
+var board;
+var game;
+
+window.onload = function () {
+    initGame();
+};
+
+var initGame = function() {
+   var cfg = {
+       draggable: true,
+       position: 'start',
+       onDrop: handleMove,
+   };
+   
+   board = new ChessBoard('gameBoard', cfg);
+   game = new Chess();
+};
+
+var handleMove = function(source, target ) {
+    var move = game.move({from: source, to: target});
+    
+    if (move === null)  return 'snapback';
+};
